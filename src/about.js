5c3ce8721dd97c04cc1b44dbc74fbc9a3534912c
Haddock <jhadddock@protonmail.com>
2023-03-09 16:37:49 -0800

    Implement contact tab

--- src/about.js
+++ src/about.js
@@ -1,6 +1,22 @@
 export default function about() {
   const home = document.createElement("div");
-  home.textContent = "About";
+  home.classList.add("tab");
+  home.innerHTML = `
+    <div class="tab hidden" id="contact">
+      <h1>Contact Info</h1>
+      <h2>Reach out to us, preferably about business</h2>
+      <ul>
+        <li>
+          Email:
+          <a href="mailto: abc@example.com">businessfoods@business.com</a>
+        </li>
+
+        <li>Phone Number: <a href="tel:123-456-7890">123-456-7890</a></li>
+      </ul>
+    </div>
+  </div>
+  `;
+
   console.log("about");
   return home;
 }
